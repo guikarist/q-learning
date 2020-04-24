f984c036881adb8c97808825c606425671ee3998
guikarist <guikarist@gmail.com>
2020-04-24 14:55:15 +0800

    Add several filters

--- q_learning.py
+++ q_learning.py
@@ -6,24 +6,38 @@ import random
 
 
 class QLearning:
-    _unhashable_error_texts = 'The state must be hashable.'
+    _unhashable_error_texts = "The state must be hashable, please use 'state_filter' in QLearning.__init__"
 
-    def __init__(self, env, epsilon, gamma, alpha, log_frequency=100, state_wrapper=None):
+    def __init__(self, env, epsilon, gamma, alpha, log_frequency=100, state_filter=None, action_filter=None,
+                 reward_filter=None, done_filter=None):
         """
         Initialization
-        :param env: A initialized gym (or gym-like) environment.
+        1. The 'env' should be gym-like.
+        2. The state of 'env' should be hashable.
+        3. The action of 'env' should be consistent with 'env.action_space.sample()'.
+        4. The reward of 'env' should be numeric type.
+        5. The done flag of 'env' should be bool type.
+
+        :param env: A initialized gym (or gym-like) environment
         :param epsilon: Epsilon
         :param gamma: Discounted factor
         :param alpha: Learning rate
         :param log_frequency: The frequency of logging
-        :param state_wrapper: The wrapper function which converts unhashable state to a hashable one
+        :param state_filter: The function which filters state of 'env' for QLearning algorithm
+        :param action_filter: The function which filters chosen action of QLearning algorithm for 'env'
+        :param reward_filter: The function which filters reward of 'env' for QLearning algorithm
+        :param done_filter: The function which filters done flag of 'env' for QLearning algorithm
         """
+
         self._env = env
         self._epsilon = epsilon
         self._gamma = gamma
         self._alpha = alpha
         self._log_frequency = log_frequency
-        self._state_wrapper = (lambda x: x) if state_wrapper is None else state_wrapper
+        self._state_wrapper = (lambda x: x) if state_filter is None else state_filter
+        self._action_filter = (lambda x: x) if action_filter is None else action_filter
+        self._reward_filter = (lambda x: x) if reward_filter is None else reward_filter
+        self._done_filter = (lambda x: x) if done_filter is None else done_filter
 
         # Initialize Q function.
         self._q_func = defaultdict(lambda: np.zeros(self._env.action_space.n))
@@ -47,8 +61,11 @@ class QLearning:
 
                     # Select action using epsilon greedy policy.
                     action = self._policy(state)
-                    next_state, reward, done, _ = self._env.step(action)
+                    next_state, reward, done, _ = self._env.step(self._action_filter(action))
                     next_state = self._state_wrapper(next_state)
+                    reward = self._reward_filter(reward)
+                    done = self._done_filter(done)
+
                     reward_sum += reward
 
                     # Update Q Function using TD target.
@@ -81,7 +98,10 @@ class QLearning:
                     self._env.render()
 
                 action = np.argmax(self._q_func[state])
-                state, reward, done, _ = self._env.step(action)
+                state, reward, done, _ = self._env.step(self._action_filter(action))
+                reward = self._reward_filter(reward)
+                done = self._done_filter(done)
+
                 state = self._state_wrapper(state)
                 reward_sum += reward
 
